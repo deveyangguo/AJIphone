8ff8c5369f75e2f3e2480a8bdaa05f27bd184c80
徐海军 <xuhaijun@joumijunmatoMacBook-Pro.local>
2012-06-19 16:18:03 +0800

    Initial Commit

---
+++ AJIphone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AJIphone
+//
+//  Created by 徐海军 on 12-6-19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

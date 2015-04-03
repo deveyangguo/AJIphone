8806988e3f191ecde4cc1e8dcdd5f1e0997d18f7
afdl6867397 <afdl6867397>
2015-04-03 20:29:28 +0800

    dddddd

--- ViewController.m
+++ ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    NSLog(@"rrrrqqqq");
+    NSLog(@"8888");
 }
 
 - (void)didReceiveMemoryWarning {
